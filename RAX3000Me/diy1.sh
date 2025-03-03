179a925426e36b98bb691553f2adf94223220c2e
keben12345 <79047578+keben12345@users.noreply.github.com>
2025-03-03 17:42:57 +0800

    Update diy1.sh

--- RAX3000Me/diy1.sh
+++ RAX3000Me/diy1.sh
@@ -23,8 +23,10 @@ git clone --depth 1 -b main https://github.com/QiYueYiya/openwrt-packages packag
 #git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 #git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # Easytier
-git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git luci-app-easytier
-git clone --depth 1 -b master https://github.com/Kiougar/luci-wrtbwmon.git luci-wrtbwmon
-mv luci-app-easytier/easytier package/easytier
-mv luci-app-easytier/luci-app-easytier package/luci-app-easytier
-rm -rf luci-app-easytier
+# git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git luci-app-easytier
+git clone --depth 1 -b master https://github.com/brvphoenix/luci-app-wrtbwmon.git luci-app-wrtbwmon
+# mv luci-app-easytier/easytier package/easytier
+# mv luci-app-easytier/luci-app-easytier package/luci-app-easytier
+# rm -rf luci-app-easytier
+mv luci-app-wrtbwmon package/luci-app-wrtbwmon
+rm -rf luci-app-wrtwmon
